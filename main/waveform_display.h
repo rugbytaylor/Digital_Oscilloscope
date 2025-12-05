8d455f0b6f6ce75e80f498bb29af1a253c57bd33
Rugby Taylor <rugbytaylor3@gmail.com>
2025-12-05 15:23:04 -0700

    good code for inital ruleset test

--- main/waveform_display.h
+++ main/waveform_display.h
@@ -7,7 +7,7 @@ void waveform_display_init(void);
 void waveform_display_add_sample(uint16_t sample);
 
 // LVGL task (keeps UI alive)
-//void lvgl_task(void *arg);
+void lvgl_task(void *arg);
 
 // Waveform update task (feeds ADC results to chart)
 void waveform_update_task(void *arg);
